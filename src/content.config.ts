2c2be94575f8c279a7b00fb7abaca5197dc7bacf
Rimarok <romaric.ruga@mailo.fr>
2025-03-03 10:00:15 +0100

    v0.1.1 : update : nebula-genesis v2.0.5 (mv slug prop to Slug)

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,9 @@ import { glob } from "astro/loaders";
 const pages = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/pages" }),
   schema: z.object({
+    Slug: z.nullable(z.string()),
     Description: z.nullable(z.string()),
+    "Mots-clés": z.nullable(z.string()),
     Name: z.string(),
     Url: z.nullable(z.string()),
   }),
